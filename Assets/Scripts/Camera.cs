1604dcff6635db57e0b9f2f543dba6a0e8ee244c
antoine odi <antoine.kenneth.odi@gmail.com>
2019-01-21 23:46:01 +1030

    Set up of folders and scene. Download of model and animation that's going to be used in game. Player script in progress. Camera script needs modification.

---
+++ Assets/Scripts/Camera.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour
+{
+
+    private GameObject m_player;
+    [SerializeField] private Vector3 m_relativePosition = Vector3.zero;
+    [SerializeField] private float m_cameraSpeed = 2f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Finds the game object with player script on it
+        m_player = FindObjectOfType<Player>().gameObject;
+
+        // Sets the current camera position as the relative position if relative position have not been set or too small 
+        if (m_relativePosition.sqrMagnitude <= 0.1f)
+            m_relativePosition = transform.position;
+
+    }
+
+    // FixedUpdate is called per time interval therefore time frame independent
+    void FixedUpdate()
+    {
+        Vector3 targetPos = m_player.transform.position + m_relativePosition;
+
+        // Move the camera position to the target position smoothly
+        transform.position = targetPos;
+        //transform.position = Vector3.Lerp (transform.position, targetPos, m_cameraSpeed * Time.fixedDeltaTime);
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterController))]
+[RequireComponent(typeof(Animator))]
+public class Player : MonoBehaviour
+{
+
+    [SerializeField] private float m_movementSpeed = 80.0f;
+    [SerializeField] private float m_turningSpeed = 180.0f;
+    [SerializeField] private int m_playerLayer = 9;
+
+    private CharacterController m_controller = null;
+    private Animator m_animator = null;
+
+    private Vector3 m_velocity = Vector3.zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Reference all components needed by the script
+        m_controller = GetComponent<CharacterController>();
+        m_animator = GetComponent<Animator>();
+
+        // Set all children transform to player layer
+        foreach (Transform child in transform)
+            child.gameObject.layer = m_playerLayer;
+    }
+
+    // FixedUpdate is called per time interval therefore time frame independent
+    void FixedUpdate()
+    {
+        //// Reads user inputs
+        //float groundVertical = Input
+    }
+
+
+        // Update is called once per frame
+        void Update()
+    {
+        
+    }
+}
